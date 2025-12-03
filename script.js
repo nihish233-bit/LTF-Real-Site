fee18ee381c5bf6ff1a5ac795a1a23c7c28a8ef7
Loyal Team Force <nihish233@gmail.com>
2025-12-03 22:28:01 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function sortLeaderboard(){
     rows.forEach((row, index) => { row.cells[0].innerText = index+1; table.appendChild(row); });
 }
 
+const slideElements = document.querySelectorAll('.slide-up');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+  });
+}, { threshold: 0.3 });
+
+slideElements.forEach(el => observer.observe(el));
